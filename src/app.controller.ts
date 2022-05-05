efc8b1891ba018a6e4bc689463ad28dddd78c199
ezuoo <juho0003@naver.com>
2022-05-05 13:58:03 +0900

    setting

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    console.log('cognito pool id : ', process.env.COGNITO_POOL_ID);
     return this.appService.getHello();
   }
 }
